e5820b42caa0b83d0eb1be198ac004c6a865b328
Alexey Bondarenko <alexeybondarenko@me.com>
2017-05-04 18:09:24 +0300

    Update index.js

--- app/common/components/Points/index.js
+++ app/common/components/Points/index.js
@@ -5,7 +5,7 @@ import styles from './styles.scss';
 
 export default withStyles(styles)(({ count = 2, active = 0 }) => (
   <ul className={styles.points}>
-    {(new Array(null, count)).map((item, i) => ( // eslint-disable-line
+    {(new Array(null, count)).map((item, i) => ( // eslint-disable-line 
       <li key={i} className={active === i ? styles.active : ''} />
     ))}
   </ul>
