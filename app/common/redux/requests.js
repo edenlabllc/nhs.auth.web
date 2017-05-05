f1327b08802a459f6ef413777fc309f4bc9e48cd
Alex <alex.hyrenko@gmail.com>
2017-05-05 12:48:48 +0300

    Registration change approve flow (#10)

--- app/common/redux/requests.js
+++ app/common/redux/requests.js
@@ -1,13 +1,14 @@
 import { handleAction, combineActions } from 'redux-actions';
-import { EMPLOYEE_API_URL } from 'config';
+import { EHEALTH_API_URL } from 'config';
 import { normalize } from 'normalizr';
 import { request } from 'schemas';
 import { invoke } from './api';
 
-export const APPROVED_STATUS = 'APPROVED';
+export const APPROVED_STATUS = 'approve';
+export const REJECT_REQUEST = 'reject';
 
 export const fetchRequestById = id => invoke({
-  endpoint: `${EMPLOYEE_API_URL}/employee_requests/${id}`,
+  endpoint: `${EHEALTH_API_URL}/employee_requests/${id}`,
   method: 'GET',
   types: ['requests/FETCH_REQUEST_REQUEST', {
     type: 'requests/FETCH_REQUEST_SUCCESS',
@@ -20,8 +21,19 @@ export const fetchRequestById = id => invoke({
   }, 'requests/FETCH_REQUEST_FAILURE'],
 });
 
+export const setRequestStatus = (id, status) => invoke({
+  endpoint: `${EHEALTH_API_URL}/employee_requests/${id}/actions/${status}`,
+  method: 'POST',
+  types: ['requests/SET_STATUS_REQUEST_REQUEST', {
+    type: 'requests/SET_STATUS_REQUEST_SUCCESS',
+    payload: (action, state, res) => res.json().then(
+      json => normalize(json.data, request)
+    ),
+  }, 'requests/SET_STATUS_REQUEST_FAILURE'],
+});
+
 export const updateRequest = (id, body) => invoke({
-  endpoint: `${EMPLOYEE_API_URL}/employee_requests/${id}`,
+  endpoint: `${EHEALTH_API_URL}/employee_requests/${id}`,
   method: 'PATCH',
   types: ['requests/UPDATE_REQUEST_REQUEST', {
     type: 'requests/UPDATE_REQUEST_SUCCESS',
