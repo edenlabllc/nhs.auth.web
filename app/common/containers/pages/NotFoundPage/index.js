df1df9f760b825bee7e7b6230ed8b094f46c1bd0
Alex <alex.hyrenko@gmail.com>
2017-05-05 11:30:53 +0300

    Customize 404 page (#8)
    * Costumize 404 page
    
    * Update index.js

--- app/common/containers/pages/NotFoundPage/index.js
+++ app/common/containers/pages/NotFoundPage/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { translate } from 'react-i18next';
+import withStyles from 'withStyles';
 
 import { H1 } from 'components/Title';
-import Button from 'components/Button';
 
-@translate()
+import styles from './styles.scss';
+
+@withStyles(styles)
 export default class NotFoundPage extends React.Component {
   render() {
-    const { t } = this.props;
-
     return (
-      <div id="not-found-page">
-        <H1>{ t('Page Not Found') }</H1>
-        <p>
-          { t('Requested page not found. Maybe you are looking for') }
-          <Button theme="link" to="/dashboard" >{ t('Go to dashboard') }</Button>.
-        </p>
+      <div className={styles.error} id="not-found-page">
+        <H1>ПОМИЛКА</H1>
+
+        <div className={styles.code}>404</div>
+
+        <H1 tag="h2">Сторінку не знайдено</H1>
+
+        <footer className={styles.footer}>
+          При виникненні питань, будь ласка, зверніться в <br />
+          <a href="mailto:support@test.com">службу підтримки</a>
+        </footer>
       </div>
     );
   }
