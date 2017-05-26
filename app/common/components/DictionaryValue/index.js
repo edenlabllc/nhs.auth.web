8822d23923f8bf43b43242d4e16e7f58aeb8dc10
Alexey Bondarenko <alexeybondarenko@me.com>
2017-05-26 15:50:35 +0300

    Remove console.log

--- app/common/components/DictionaryValue/index.js
+++ app/common/components/DictionaryValue/index.js
@@ -4,6 +4,6 @@ import getFn from 'lodash/get';
 import values from './values.json';
 
 const valuesObject = keyByFn(values.data, 'name');
-console.log(valuesObject);
+
 export default ({ dictionary, value }) =>
   <span>{getFn(valuesObject[dictionary], `values[${value}]`, value)}</span>;
