ac4caa56c26b5617a3f46c40a03085c77a31ea35
ivchenkodima <ivchenkodima@gmail.com>
2017-11-10 15:06:00 +0200

    Add debug for specifying cloudflare loops

--- app/common/containers/pages/AcceptPage/index.js
+++ app/common/containers/pages/AcceptPage/index.js
@@ -46,6 +46,7 @@ export default class AcceptPage extends React.Component {
       scope,
       redirectUri: query.redirect_uri,
     }).then(({ payload, error }) => {
+      console.log('debug: ', payload, error);
       if (error) {
         return this.setState({
           error: Object.entries(payload.response.error).map(([key, value]) => ({
