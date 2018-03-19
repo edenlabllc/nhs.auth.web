31a1b17b7f2989bda8549107872bf42331d7644a
Myroslav Kiurchev <kiurchv@gmail.com>
2018-03-20 00:24:59 +0200

    chore: temporarily hardcode runtime variables

--- src/config.js
+++ src/config.js
@@ -1,19 +1,4 @@
-let config = {};
-
-export const API_URL =
-  config.API_ENDPOINT ||
-  process.env.API_ENDPOINT ||
-  "https://dev.ehealth.world";
-
-export const AUTH_URL =
-  config.API_ENDPOINT ||
-  process.env.AUTH_ENDPOINT ||
-  "https://dev.ehealth.world";
-
-export const CLIENT_ID =
-  config.CLIENT_ID ||
-  process.env.CLIENT_ID ||
-  "e2e1d2c8-9bac-43c6-adef-83239940b30a";
-
-export const AUTH_COOKIE_NAME =
-  config.AUTH_COOKIE_NAME || process.env.AUTH_COOKIE_NAME || "token";
+export const API_URL = "https://dev.ehealth.world";
+export const AUTH_URL = "https://dev.ehealth.world";
+export const CLIENT_ID = "e2e1d2c8-9bac-43c6-adef-83239940b30a";
+export const AUTH_COOKIE_NAME = "token";
